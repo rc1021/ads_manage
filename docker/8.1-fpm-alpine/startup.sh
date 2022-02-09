9f3d2b521eb1d413b9554386fdb4b9595703b46b
rc1021 <rc.701021@gmail.com>
2022-02-09 15:15:37 +0800

    fix up deployment

--- docker/8.1-fpm-alpine/startup.sh
+++ docker/8.1-fpm-alpine/startup.sh
@@ -3,8 +3,9 @@
 if [ ! -f /var/www/html/.env ]
 then
     cp /var/www/html/.env.example /var/www/html/.env
+    php /var/www/html/artisan key:generate
 fi
 
-php /var/www/html/artisan key:generate
+php /var/www/html/artisan migrate
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
